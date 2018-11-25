ed9b6c4d48e713e9a77f2bfb7b4fe284732e17b0
Wil Cook <wil.cook.is@gmail.com>
2018-11-25 16:28:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -54,7 +54,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"].to_s
+          :languages => ["C"].join
         }
      }
   programmer_hash[:dennis_ritchie][:languages]
